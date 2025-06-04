d640acd65471d5625f733d46c1c97fa38708b289
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2025-06-04 18:42:26 +0200

    forgt to calll rjd3toolkti

--- R/workspace.R
+++ R/workspace.R
@@ -154,6 +154,25 @@ jws_refresh <- function(jws,
 #'
 #' @examples
 #'
+#' library("rjd3toolkit")
+#'
+#' # French calendar
+#' french_calendar <- national_calendar(
+#'     days = list(
+#'         fixed_day(7, 14), # Bastille Day
+#'         fixed_day(5, 8, validity = list(start = "1982-05-08")), # End of 2nd WW
+#'         special_day("NEWYEAR"),
+#'         special_day("CHRISTMAS"),
+#'         special_day("MAYDAY"),
+#'         special_day("EASTERMONDAY"),
+#'         special_day("ASCENSION"),
+#'         special_day("WHITMONDAY"),
+#'         special_day("ASSUMPTION"),
+#'         special_day("ALLSAINTSDAY"),
+#'         special_day("ARMISTICE")
+#'     )
+#' )
+#'
 #' # Load a Workspace
 #' file <- system.file("workspaces", "workspace_test.xml", package = "rjd3workspace")
 #' jws <- jws_open(file)
