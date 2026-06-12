d5db013210a83819c8f78cc4f48e437512fddbe4
palatej <palatejean@gmail.com>
2026-06-12 09:39:51 +0200

    Code cleanup

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 #' @importFrom rJava .jpackage .jcall .jnull .jarray .jevalArray .jcast .jcastToArray .jinstanceof is.jnull .jnew .jclass
-#' @import rjd3x13 rjd3tramoseats rjd3providers rjd3toolkit
+#' @import rjd3toolkit
 NULL
 
 
--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,9 @@
 #' @include utils.R
 #' @importFrom rjd3jars reload_safactories reload_tsproviders check_java_version
+#' @importFrom rjd3tramoseats tramoseats_dictionary
+#' @importFrom rjd3x13 x13_dictionary
+#' @importFrom rjd3providers spreadsheet_name
+#' @importFrom rjd3toolkit dictionary
 NULL
 
 #' @importFrom rJava .jpackage .jaddClassPath
