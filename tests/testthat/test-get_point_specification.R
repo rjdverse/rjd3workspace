b2bcd00a54a1f0cb87ad47755dcde88da3f61369
Eulalie Delaune <eulalie.delaune@eleve.ensai.fr>
2026-07-15 15:12:58 +0200

    fixed issue about java versions in test and fixed links in news

--- tests/testthat/test-get_point_specification.R
+++ tests/testthat/test-get_point_specification.R
@@ -2,10 +2,12 @@ library("rjd3toolkit")
 library("rjd3x13")
 
 test_that("get_point_specification works", {
+    skip_if_not(rjd3jars::check_java_version())
+
     # Case 1
     jws <- jws_new()
-    jsap <- jws_sap_new(jws, "sap1")
-    add_sa_item(jsap, "test", ABS$X0.2.09.10.M, x13_spec())
+    jsap <- jws_sap_new(jws, name = "sap1")
+    add_sa_item(jsap, "test", rjd3toolkit::ABS$X0.2.09.10.M, x13_spec())
     jsai <- jsap_sai(jsap, 1L)
 
     expect_no_error({
